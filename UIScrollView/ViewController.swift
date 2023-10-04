4ee57079628981f31a8609cf128649555c27e051
hinakko <131275914+hinakko02@users.noreply.github.com>
2023-10-05 07:43:34 +0900

    Label表示、View表示されない

---
+++ UIScrollView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIScrollView
+//
+//  Created by Hina on 2023/10/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
